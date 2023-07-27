5235c35201cba30ba8c9768d144bcedac7c886b6
notand <urzua.fernando.f@gmail.com>
2023-07-27 12:58:02 -0600

    responsive

--- animated-background-to-nav/script.js
+++ animated-background-to-nav/script.js
@@ -2,3 +2,10 @@ window.addEventListener("scroll", ()=>{
     const header = document.querySelector('header')
     header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+const nav = document.querySelector('nav')
+
+document.querySelector('.toggle').onclick = function(){
+    this.classList.toggle('active')
+    nav.classList.toggle('active')
+}
